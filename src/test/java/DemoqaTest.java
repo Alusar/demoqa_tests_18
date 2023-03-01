2af13a5439db54044ee428d2e19004d99545b55e
Sergei Sokolov <fessnvm@gmail.com>
2023-03-01 16:29:47 +0300

    try2

--- src/test/java/DemoqaTest.java
+++ src/test/java/DemoqaTest.java
@@ -1,4 +1,6 @@
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -7,40 +9,46 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DemoqaTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
     @Test
     public void toolsQaForm() {
         open("https://demoqa.com/automation-practice-form");
         Selenide.executeJavaScript("$('#fixedban').remove()");
         Selenide.executeJavaScript("$('footer').remove()");
 
-        $("#firstName").setValue("Oleg");
-        $("#lastName").setValue("Petrov");
-        $("#userEmail").setValue("PetrovOleg@mail.ru");
+        $("#firstName").setValue("ser");
+        $("#lastName").setValue("sergov");
+        $("#userEmail").setValue("sergov@serg.ru");
         $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("9168130220");
+        $("#userNumber").setValue("9205240000");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-dropdown-container").$(byText("March")).click();
         $(".react-datepicker__year-dropdown-container").$(byText("1980")).click();
         $(".react-datepicker__month-container").$(byText("13")).click();
         $("#subjectsInput").setValue("English").pressEnter();
-        $("#hobbies-checkbox-3").parent().$(byText("Music")).click();
-        $("#uploadPicture").uploadFromClasspath("java/resources/picture/14.png");
-        $("#currentAddress").setValue("Victory cq 5");
+        $("#hobbies-checkbox-2").parent().$(byText("Reading")).click();
+        $("#currentAddress").setValue("Perumov");
         $("#state").click();
-        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
-        $("#stateCity-wrapper").$(byText("Agra")).click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
+
         $(".modal-open").shouldHave(text("Thanks for submitting the form"));
-        $(".modal-body").shouldHave(text("Oleg Petrov"));
-        $(".modal-body").shouldHave(text("PetrovOleg@mail.ru"));
+        $(".modal-body").shouldHave(text("ser sergov"));
+        $(".modal-body").shouldHave(text("sergov@serg.ru"));
         $(".modal-body").shouldHave(text("Male"));
-        $(".modal-body").shouldHave(text("9168130220"));
+        $(".modal-body").shouldHave(text("9205240000"));
         $(".modal-body").shouldHave(text("13 March,1980"));
         $(".modal-body").shouldHave(text("English"));
-        $(".modal-body").shouldHave(text("Music"));
-        $(".modal-body").shouldHave(text("Victory cq 5"));
-        $(".modal-body").shouldHave(text("Uttar Pradesh Agra"));
+        $(".modal-body").shouldHave(text("Reading"));
+        $(".modal-body").shouldHave(text("Perumov"));
+        $(".modal-body").shouldHave(text("NCR Delhi"));
         $("#closeLargeModal").click();
 
     }
